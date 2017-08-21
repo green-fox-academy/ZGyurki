e4a9cbeb848742b234deab1154aada394a5abf70
ZGyurki <z.gyurki@gmail.com>
2017-08-21 10:16:57 +0200

    ppt, txt

--- week-02_ba/day-2/codeblocks_first_practise/01.c
+++ week-02_ba/day-2/codeblocks_first_practise/01.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	int a = 3;
-	a += 10;
-	printf("%d", a);
-	// make it bigger by 10
-	return 0;
+int main()
+{
+    char str[] = "This a sample string1.This a sample string2.This a sample string3.\n";
+    char buff[3][50];
+    char * pch;
+
+    pch = strtok(str, ".");
+
+    int i = 0;
+    while (pch != NULL) {
+        strcpy(buff[i], pch);
+        pch = strtok(NULL, ".");
+        i++;
+    }
+    for (i = 0; i < 3; i++){
+        printf ("%s\n", buff[i]);
+        pch = strtok(buff[i], " ");
+        while (pch != NULL) {
+            printf("%s\n", pch);
+            pch = strtok(NULL, " ");
+        }
+    }
+
+    printf("\nFinally done AND cool!\n");
+    printf("%d", sizeof(buff) / sizeof(buff[0]));
+    return 0;
 }
